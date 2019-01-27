e8d3fdcd3fe2a17de7fd1e033c1c5123c40660a3
Bill Sheng <billxsheng@gmail.com>
2019-01-27 00:18:17 -0500

    bills tile

--- src/components/Listings/listings.js
+++ src/components/Listings/listings.js
@@ -8,8 +8,21 @@ const Listings = (props) => {
 
        </div>
        <div className="fl w-70 right-block">
-            <div className="game-tile">
-
+            <div className="game-tile grow">
+                <div className="tile-title"><h3>SuperBowl LLI: Sunday, February 3rd, 2019</h3></div>
+                <div className="versus-content">
+                    <div className="fl w-40 team-first">
+                        <img src={require("../../assets/neweng.png")} />
+                        <h4>New England Patriots</h4>
+                    </div>
+                    <div className="fl w-20 versus">
+                        <h1>VS</h1>
+                    </div>
+                    <div className="fl w-40 team-last">
+                        <img src={require("../../assets/rams.png")} />
+                        <h4>Los Angeles Rams</h4>
+                    </div>
+                </div>
             </div>
        </div>
     </div>
