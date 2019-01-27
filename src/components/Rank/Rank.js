4b41f98fdfcd05137e69bdeb97503f6cfda42787
patrick <patrickduu@gmail.com>
2019-01-27 01:03:59 -0500

    userpage

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -7,28 +7,36 @@ export default class Rank extends React.Component {
 
   render() {
     return (
-<div>
-      <div className="flex justify-center">
-        <div className="outline w-20 pa3 mr2">
-          <img className="iconpic" src="./bill.jpg"></img>
+      <div>
+        <div className="flex justify-center">
+          <div className=" w-20 pa3 mr2">
+            <img src={require("./bill.jpg")} />
+          </div>
+          <div class=" w-40 pa3 mr2">
+            <h1 className="black f2">Bill Sheng</h1>
+            <h3 className="blue">Die Hard New England Patriots Fan</h3>
+          </div>
+          <div className=" w-20 pa3 mr2">
+            <p className="black">Age: 24 <br></br> Location: Markham, Ontario <br></br><br></br> Win/Loss Ratio: 24/48 (50%) <br></br>
+              Winnings: $621.56
+          </p>
+          </div>
         </div>
-        <div class="outline w-40 pa3 mr2">
-          <h1 className="white f2">Bill Sheng</h1>
-          <h3 className="white">Die Hard New England Patriots Fan</h3>
+        <div className="flex justify-center">
+          <div className=" w-80 pa3 mr2">
+            <h3 className="brown"> Betting History </h3>
+            <h3 className="yellow"> Bill put the fat ass card here </h3>
+          </div>
         </div>
-        <div className="outline w-20 pa3 mr2">
-          <p className="white">Age: 24 <br></br> Location: Markham, Ontario <br></br><br></br> Win/Loss Ratio: 24/48 (50%) <br></br>
-            Winnings: $621.56
-          </p>  
+        <div className="flex justify-center">
+          <div className=" w-80 pa3 mr2" id="btn-row">
+            <a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-black"  href="#0">Listings</a>
+          </div>
         </div>
+
+
       </div>
-    <div className="flex lf">
-    <h3 className="white"> Betting History </h3>
-    </div>
-      
 
-    </div>
-
-            );
-          }
-    }
+    );
+  }
+}
